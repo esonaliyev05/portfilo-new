b6a8e4401437ab8acf714c65c5e23f5fc53fa7b9
esonaliyev05 <esonaliyevalyorbek911@gmail.com>
2026-01-16 13:43:24 +0500

    Designed and Developed by Vinod Jangid

--- main.js
+++ main.js
@@ -78,10 +78,7 @@ window.addEventListener("scroll", () => {
         t.classList.contains(e) && t.classList.add("activeThistab");
     });
 }),
-  console.log(
-    "%c Designed and Developed by Vinod Jangid ",
-    "background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;"
-  );
+ 
 let mybutton = document.getElementById("backtotopbutton");
 function scrollFunction() {
   document.body.scrollTop > 400 || document.documentElement.scrollTop > 400
